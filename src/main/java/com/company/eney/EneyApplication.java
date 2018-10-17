7eaace4c91061c284f27f32d66372004de991d71
hanseulleee <gkstmvdl@gmail.com>
2018-10-17 13:26:45 +0900

    first commit

---
+++ src/main/java/com/company/eney/EneyApplication.java
@@ -0,0 +1,12 @@
+package com.company.eney;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class EneyApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(EneyApplication.class, args);
+    }
+}
